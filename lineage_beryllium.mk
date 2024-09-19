6cdcd6696c5705dbe2e7888c99e45b89bfde8262
Dheeraj R Shrivatsa <dheerajshrivatsa@gmail.com>
2024-09-19 20:16:34 +0530

    beryllium: Update PRODUCT_BUILD_PROP_OVERRIDES format
    beryllium: Update PRODUCT_BUILD_PROP_OVERRIDES format
    
    * Ref https://review.lineageos.org/c/LineageOS/android_build_soong/+/401215
    
    Signed-off-by: Dheeraj3031A <dheerajshrivatsa@gmail.com>
    Co-Authored-By: Clarence K <clarencekuiek@proton.me>

--- lineage_beryllium.mk
+++ lineage_beryllium.mk
@@ -21,8 +21,8 @@ PRODUCT_SYSTEM_NAME := beryllium
 BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V12.0.3.0.QEJMIXM:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V12.0.3.0.QEJMIXM release-keys" \
-    TARGET_PRODUCT="beryllium"
+    BuildDesc=$(call normalize-path-list, beryllium-user 10 QKQ1.190828.002 V12.0.3.0.QEJMIXM release-keys") \
+    DeviceProduct=beryllium
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
