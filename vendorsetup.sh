bd24f54b08f2ad8c56473314414e49985ae64f05
Dheeraj R Shrivatsa <dheerajshrivatsa@gmail.com>
2024-09-27 20:08:48 +0530

    beryllium: InfinityX System-ext

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,7 +4,7 @@ git clone https://github.com/Dheeraj3031A/proprietary_vendor_xiaomi_beryllium.gi
 
 # Kernel Tree
 rm -rf kernel/xiaomi/beryllium
-git clone https://github.com/Dheeraj3031A/kernel_xiaomi_beryllium.git -b 14.0 kernel/xiaomi/beryllium --depth=1
+git clone https://github.com/Dheeraj3031A/kernel_xiaomi_beryllium.git -b infinity-ksu kernel/xiaomi/beryllium --depth=1
 
 # MiCam
 rm -rf vendor/miuicamera
@@ -17,3 +17,7 @@ git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-21
 # Viper4FX
 rm -rf packages/apps/ViPER4AndroidFX
 git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git -b v4a packages/apps/ViPER4AndroidFX
+
+# KSU
+cd kernel/xiaomi/beryllium
+curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s v0.9.5
