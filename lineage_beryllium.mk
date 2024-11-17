fa721f6f7498c343ebf36dd946219a06e27de610
Lup Gabriel <gwolf2u.website@gmail.com>
2024-11-17 16:25:14 +0200

    beryllium: Adjust override prop key names

--- lineage_beryllium.mk
+++ lineage_beryllium.mk
@@ -18,13 +18,13 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_SYSTEM_NAME := beryllium
-
 BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V12.0.3.0.QEJMIXM:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V12.0.3.0.QEJMIXM release-keys" \
-    TARGET_PRODUCT="beryllium"
+    BuildDesc="beryllium-user 10 QKQ1.190828.002 V12.0.3.0.QEJMIXM release-keys" \
+    BuildFingerprint=Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V12.0.3.0.QEJMIXM:user/release-keys \
+    DeviceName=beryllium \
+    SystemName=beryllium
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
