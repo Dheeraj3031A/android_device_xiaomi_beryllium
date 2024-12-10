8bb954a8c2cba339d8966afebc6e534f445890ad
Dheeraj R Shrivatsa <dheerajshrivatsa@gmail.com>
2024-12-10 22:28:05 +0530

    beryllium: Add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,19 @@
+# Vendor Tree
+rm -rf vendor/xiaomi/beryllium
+git clone https://github.com/Dheeraj3031A/proprietary_vendor_xiaomi_beryllium.git -b lineage-20 vendor/xiaomi/beryllium --depth=1
+
+# Kernel Tree
+rm -rf kernel/xiaomi/beryllium
+git clone https://github.com/Legendleo90/kernel_xiaomi_beryllium.git kernel/xiaomi/beryllium --depth=1
+
+# MiCam
+rm -rf vendor/miuicamera
+git clone https://github.com/Legendleo90/vendor_miuicamera.git vendor/miuicamera --depth=1
+
+# Viper4FX
+rm -rf packages/apps/ViPER4AndroidFX
+git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git -b v4a packages/apps/ViPER4AndroidFX
+
+# BCR
+rm -rf vendor/bcr
+git clone https://github.com/Chaitanyakm/vendor_bcr.git -b main vendor/bcr --depth=1
